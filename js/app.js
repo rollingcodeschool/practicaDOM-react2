348619bf7dcf2dbcb15be50e81e0dcf2c15fb82e
Emilse Arias <emilsearias@gmail.com>
2024-11-07 10:04:13 -0300

    agregamos ejemplos de getElementByID, getElementByTagName, querySelector y querySelectorAll

---
+++ js/app.js
@@ -0,0 +1,16 @@
+function cambiarTitulo(){
+    console.log('desde la funcion cambiar titulo')
+    //traer mi titulo h1
+    // const tituloPrincipal = document.getElementsByTagName('h1')
+    const tituloPrincipal = document.querySelector('h1')
+    console.log(tituloPrincipal);
+    tituloPrincipal.innerText = 'Hola mundo 🌍'
+    tituloPrincipal.className = 'text-info display-4'
+    console.log(tituloPrincipal)
+    // const parrafos = document.getElementById('parrafoDestacado')
+    const parrafos = document.querySelector('#parrafoDestacado')
+    console.log(parrafos)
+    // const botones = document.getElementsByClassName('btn-outline-info')
+    const botones = document.querySelectorAll('.btn')
+    console.log(botones)
+}
